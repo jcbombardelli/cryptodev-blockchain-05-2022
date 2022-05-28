a5536de92d77c598f293e178fb4ef0322b4309ca
JC Bombardelli <jc.bombardelli@live.com>
2022-05-28 12:51:43 -0300

    Dia 10 - Block & Chain
    A dupla sertaneja mais famosa do mundo

--- dia-10/index.js
+++ dia-10/index.js
@@ -1,4 +1,17 @@
 const readline = require('readline-sync')
+const Chain = require('./src/chain')
+
+
+//Simulação do uso do Menu
+const chain = new Chain()
+chain.addBlock({ amount: 33 }) //Posicao 1
+chain.addBlock({ amount: 10 }) //Posicao index 2
+chain.addBlock({ amount: 44, message: 'BTC > All' }) //Posicao Index 3
+
+//Hackerman
+chain.instance[1].data.amount = 9001
+chain.instance[1].hash = '579e6e623c3e1d099c2b8c93c929b6c14432fa9daa87e4a699b11571d3071278'
+
 
 function main() {
   clear()
@@ -12,11 +25,15 @@ function main() {
       case 1:
         const amount = readline.questionInt('Amount: ')
         const message = readline.question('Message: ')
+
+        chain.addBlock({ amount, message }) //Cria um novo bloco e adiciona na cadeia
+
         console.log('Block added!')
         clear(true)
         break;
       case 2:
         console.log('Blocks List!')
+        chain.print() //Lista cadeia de blocos a partir da instancia
         clear(true)
         break;
       case 0:
